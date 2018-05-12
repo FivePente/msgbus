36f899d02a68de7e069846437af85d69a10fcd28
James Mills <prologic@shortcircuit.net.au>
2018-05-12 12:32:22 -0700

    Fixed drop rate for subscriber listeners by adding buffering to channels (configurable)

--- cmd/msgbusd/main.go
+++ cmd/msgbusd/main.go
@@ -16,6 +16,7 @@ func main() {
 		version        bool
 		debug          bool
 		bind           string
+		bufferLength   int
 		maxQueueSize   int
 		maxPayloadSize int
 	)
@@ -25,6 +26,7 @@ func main() {
 
 	flag.StringVar(&bind, "bind", ":8000", "interface and port to bind to")
 
+	flag.IntVar(&bufferLength, "buffer-length", msgbus.DefaultBufferLength, "buffer length")
 	flag.IntVar(&maxQueueSize, "max-queue-size", msgbus.DefaultMaxQueueSize, "maximum queue size")
 	flag.IntVar(&maxPayloadSize, "max-payload-size", msgbus.DefaultMaxPayloadSize, "maximum payload size")
 
@@ -42,6 +44,7 @@ func main() {
 	}
 
 	opts := msgbus.Options{
+		BufferLength:   bufferLength,
 		MaxQueueSize:   maxQueueSize,
 		MaxPayloadSize: maxPayloadSize,
 		WithMetrics:    true,
